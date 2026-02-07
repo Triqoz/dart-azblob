9db96220a842e0db6b923a6c1845f0da72d7dca7
Floyd Butselaar <floyd@baseflow.com>
2026-02-07 15:51:51 +0100

    WIP to be ammended

--- example/example.dart
+++ example/example.dart
@@ -1,6 +1,6 @@
 import 'package:azblob/azblob.dart';
 
-main() async {
+Future<void> main() async {
   var storage = AzureStorage.parse('your connection string');
   await storage.putBlob('/yourcontainer/yourfile.txt', body: 'Hello, world.');
 }
--- lib/src/azblob_base.dart
+++ lib/src/azblob_base.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:typed_data';
+import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'package:crypto/crypto.dart' as crypto;
+import 'package:http/http.dart';
 import 'package:http_parser/http_parser.dart';
 
 /// Blob type
@@ -23,33 +25,42 @@ class AzureStorageException implements Exception {
   AzureStorageException(this.message, this.statusCode, this.headers);
 }
 
+/// Azure Storage Exception
+class ConnectionStringParseException implements Exception {
+  final String message;
+  final String connectionString;
+
+  ConnectionStringParseException(this.message, this.connectionString);
+}
+
 /// Azure Storage Client
 class AzureStorage {
   late Map<String, String> config;
-  late Uint8List accountKey;
+  late Uint8List accountKeyBytes;
 
-  static final String defaultEndpointsProtocol = 'DefaultEndpointsProtocol';
-  static final String endpointSuffix = 'EndpointSuffix';
-  static final String accountName = 'AccountName';
-
-  // ignore: non_constant_identifier_names
-  static final String AccountKey = 'AccountKey';
+  static const String defaultEndpointsProtocol = 'DefaultEndpointsProtocol';
+  static const String endpointSuffix = 'EndpointSuffix';
+  static const String accountName = 'AccountName';
+  static const String accountKey = 'AccountKey';
 
   /// Initialize with connection string.
   AzureStorage.parse(String connectionString) {
     try {
-      var m = <String, String>{};
-      var items = connectionString.split(';');
-      for (var item in items) {
-        var i = item.indexOf('=');
-        var key = item.substring(0, i);
-        var val = item.substring(i + 1);
+      Map<String, String> m = <String, String>{};
+      final List<String> items = connectionString.split(';');
+      for (final String item in items) {
+        final int i = item.indexOf('=');
+        final String key = item.substring(0, i);
+        final String val = item.substring(i + 1);
         m[key] = val;
       }
       config = m;
-      accountKey = base64Decode(config[AccountKey]!);
-    } catch (e) {
-      throw Exception('Parse error.');
+      accountKeyBytes = base64Decode(config[accountKey]!);
+    } on Exception catch (e) {
+      throw throw ConnectionStringParseException(
+        connectionString,
+        'Failed to parse connection string: $e',
+      );
     }
   }
 
@@ -59,19 +70,21 @@ class AzureStorage {
   }
 
   Uri uri({String path = '/', Map<String, String>? queryParameters}) {
-    var blobEndpoint = config['BlobEndpoint'];
+    final String? blobEndpoint = config['BlobEndpoint'];
+    // TODO(Triqoz): This needs to be revisited in the context of azurite.
     if (blobEndpoint != null) {
       // Parse from explicit endpoint (like Azurite's BlobEndpoint)
-      var base = Uri.parse(blobEndpoint);
+      final Uri base = Uri.parse(blobEndpoint);
       return base.replace(
         path: '${base.path}$path',
         queryParameters: queryParameters,
       );
     }
 
-    var scheme = config[defaultEndpointsProtocol] ?? 'https';
-    var suffix = config[endpointSuffix] ?? 'core.windows.net';
-    var name = config[accountName];
+    final String scheme = config[defaultEndpointsProtocol] ?? 'https';
+    final String suffix = config[endpointSuffix] ?? 'core.windows.net';
+    final String? name = config[accountName];
+    // TODO(Triqoz): With name being nullable this might go very wrong for azurite
     return Uri(
       scheme: scheme,
       host: '$name.blob.$suffix',
@@ -81,7 +94,7 @@ class AzureStorage {
   }
 
   String _canonicalHeaders(Map<String, String> headers) {
-    var keys = headers.keys
+    final List<String> keys = headers.keys
         .where((i) => i.startsWith('x-ms-'))
         .map((i) => '$i:${headers[i]}\n')
         .toList();
@@ -93,42 +106,44 @@ class AzureStorage {
     if (items.isEmpty) {
       return '';
     }
-    var keys = items.keys.toList();
-    keys.sort();
+    final List<String> keys = items.keys.toList()..sort();
     return keys.map((i) => '\n$i:${items[i]}').join();
   }
 
   void sign(http.Request request) {
     request.headers['x-ms-date'] = formatHttpDate(DateTime.now());
     request.headers['x-ms-version'] = '2019-12-12';
-    var ce = request.headers['Content-Encoding'] ?? '';
-    var cl = request.headers['Content-Language'] ?? '';
-    var cz = request.contentLength == 0 ? '' : '${request.contentLength}';
-    var cm = request.headers['Content-MD5'] ?? '';
-    var ct = request.headers['Content-Type'] ?? '';
-    var dt = request.headers['Date'] ?? '';
-    var ims = request.headers['If-Modified-Since'] ?? '';
-    var imt = request.headers['If-Match'] ?? '';
-    var inm = request.headers['If-None-Match'] ?? '';
-    var ius = request.headers['If-Unmodified-Since'] ?? '';
-    var ran = request.headers['Range'] ?? '';
-    var chs = _canonicalHeaders(request.headers);
-    var crs = _canonicalResources(request.url.queryParameters);
-    var name = config[accountName];
-    var path = request.url.path;
-    var sig =
+    final String ce = request.headers['Content-Encoding'] ?? '';
+    final String cl = request.headers['Content-Language'] ?? '';
+    final String cz = request.contentLength == 0
+        ? ''
+        : '${request.contentLength}';
+    final String cm = request.headers['Content-MD5'] ?? '';
+    final String ct = request.headers['Content-Type'] ?? '';
+    final String dt = request.headers['Date'] ?? '';
+    final String ims = request.headers['If-Modified-Since'] ?? '';
+    final String imt = request.headers['If-Match'] ?? '';
+    final String inm = request.headers['If-None-Match'] ?? '';
+    final String ius = request.headers['If-Unmodified-Since'] ?? '';
+    final String ran = request.headers['Range'] ?? '';
+    final String chs = _canonicalHeaders(request.headers);
+    final String crs = _canonicalResources(request.url.queryParameters);
+    final String? name = config[accountName];
+    final String path = request.url.path;
+    final String sig =
         '${request.method}\n$ce\n$cl\n$cz\n$cm\n$ct\n$dt\n$ims\n$imt\n$inm\n$ius\n$ran\n$chs/$name$path$crs';
-    var mac = crypto.Hmac(crypto.sha256, accountKey);
-    var digest = base64Encode(mac.convert(utf8.encode(sig)).bytes);
-    var auth = 'SharedKey $name:$digest';
+    final Hmac mac = crypto.Hmac(crypto.sha256, accountKeyBytes);
+    final String digest = base64Encode(mac.convert(utf8.encode(sig)).bytes);
+    final String auth = 'SharedKey $name:$digest';
     request.headers['Authorization'] = auth;
-    //print(sig);
   }
 
   (String, String?) _splitPathSegment(String path) {
     final p = path.startsWith('/') ? path.substring(1) : path;
     final i = p.indexOf('/');
-    if (i < 0 || p.length < i + 2) return (p, null);
+    if (i < 0 || p.length < i + 2) {
+      return (p, null);
+    }
     return (p.substring(0, i), p.substring(i + 1));
   }
 
@@ -141,15 +156,15 @@ class AzureStorage {
   ///
   /// You cat use `await response.stream.bytesToString();` to get blob listing as XML format.
   Future<http.StreamedResponse> listBlobsRaw(String path) async {
-    var (container, rest) = _splitPathSegment(path);
-    var request = http.Request(
+    final (String container, String? rest) = _splitPathSegment(path);
+    final Request request = http.Request(
       'GET',
       uri(
         path: container,
         queryParameters: {
           "restype": "container",
           "comp": "list",
-          if (rest != null) "prefix": rest,
+          "prefix": ?rest,
         },
       ),
     );
@@ -159,43 +174,44 @@ class AzureStorage {
 
   /// Get Blob.
   Future<http.StreamedResponse> getBlob(String path) async {
-    var request = http.Request('GET', uri(path: path));
+    final Request request = http.Request('GET', uri(path: path));
     sign(request);
     return request.send();
   }
 
   /// Delete Blob
   Future<http.StreamedResponse> deleteBlob(String path) async {
-    var request = http.Request('DELETE', uri(path: path));
+    final Request request = http.Request('DELETE', uri(path: path));
     sign(request);
     return request.send();
   }
 
   String _signedExpiry(DateTime? expiry) {
-    var str = (expiry ?? DateTime.now().add(const Duration(hours: 1)))
-        .toUtc()
-        .toIso8601String();
-    return '${str.substring(0, str.indexOf('.'))}Z';
+    final String expiryString =
+        (expiry ?? DateTime.now().add(const Duration(hours: 1)))
+            .toUtc()
+            .toIso8601String();
+    return '${expiryString.substring(0, expiryString.indexOf('.'))}Z';
   }
 
   /// Get Blob Link.
   Future<Uri> getBlobLink(String path, {DateTime? expiry}) async {
-    var signedPermissions = 'r';
-    var signedStart = '';
-    var signedExpiry = _signedExpiry(expiry);
-    var signedIdentifier = '';
-    var signedVersion = '2012-02-12';
-    var name = config[accountName];
-    var canonicalizedResource = '/$name$path';
-    var str =
+    final String signedPermissions = 'r';
+    final String signedStart = '';
+    final String signedExpiry = _signedExpiry(expiry);
+    final String signedIdentifier = '';
+    final String signedVersion = '2012-02-12';
+    final String? name = config[accountName];
+    final String canonicalizedResource = '/$name$path';
+    final String sigStr =
         '$signedPermissions\n'
         '$signedStart\n'
         '$signedExpiry\n'
         '$canonicalizedResource\n'
         '$signedIdentifier\n'
         '$signedVersion';
-    var mac = crypto.Hmac(crypto.sha256, accountKey);
-    var sig = base64Encode(mac.convert(utf8.encode(str)).bytes);
+    final Hmac mac = crypto.Hmac(crypto.sha256, accountKeyBytes);
+    final String sig = base64Encode(mac.convert(utf8.encode(sigStr)).bytes);
     return uri(
       path: path,
       queryParameters: {
@@ -220,14 +236,26 @@ class AzureStorage {
     BlobType type = BlobType.blockBlob,
     Map<String, String>? headers,
   }) async {
-    var request = http.Request('PUT', uri(path: path));
+    if (body == null && bodyBytes == null) {
+      throw ArgumentError('body or bodyBytes is required.');
+    }
+
+    if (body != null && bodyBytes != null) {
+      throw ArgumentError(
+        'body and bodyBytes are exclusive, pass only one of them.',
+      );
+    }
+
+    final Request request = http.Request('PUT', uri(path: path));
     request.headers['x-ms-blob-type'] = type.displayName;
     if (headers != null) {
       headers.forEach((key, value) {
         request.headers['x-ms-meta-$key'] = value;
       });
     }
-    if (contentType != null) request.headers['content-type'] = contentType;
+    if (contentType != null) {
+      request.headers['content-type'] = contentType;
+    }
     if (type == BlobType.blockBlob) {
       if (bodyBytes != null) {
         request.bodyBytes = bodyBytes;
@@ -238,17 +266,17 @@ class AzureStorage {
       request.body = '';
     }
     sign(request);
-    var res = await request.send();
-    if (res.statusCode == 201) {
-      await res.stream.drain();
-      if (type == BlobType.appendBlob && (body != null || bodyBytes != null)) {
-        await appendBlock(path, body: body, bodyBytes: bodyBytes);
-      }
-      return;
+    final StreamedResponse res = await request.send();
+
+    if (res.statusCode != 201) {
+      final String message = await res.stream.bytesToString();
+      throw AzureStorageException(message, res.statusCode, res.headers);
     }
 
-    var message = await res.stream.bytesToString();
-    throw AzureStorageException(message, res.statusCode, res.headers);
+    await res.stream.drain();
+    if (type == BlobType.appendBlob && (body != null || bodyBytes != null)) {
+      await appendBlock(path, body: body, bodyBytes: bodyBytes);
+    }
   }
 
   /// Append block to blob.
@@ -257,7 +285,17 @@ class AzureStorage {
     String? body,
     Uint8List? bodyBytes,
   }) async {
-    var request = http.Request(
+    if (body == null && bodyBytes == null) {
+      throw ArgumentError('body or bodyBytes is required.');
+    }
+
+    if (body != null && bodyBytes != null) {
+      throw ArgumentError(
+        'body and bodyBytes are exclusive, pass only one of them.',
+      );
+    }
+
+    final Request request = http.Request(
       'PUT',
       uri(path: path, queryParameters: {'comp': 'appendblock'}),
     );
@@ -267,13 +305,13 @@ class AzureStorage {
       request.body = body;
     }
     sign(request);
-    var res = await request.send();
+    final StreamedResponse res = await request.send();
     if (res.statusCode == 201) {
       await res.stream.drain();
       return;
     }
 
-    var message = await res.stream.bytesToString();
+    final String message = await res.stream.bytesToString();
     throw AzureStorageException(message, res.statusCode, res.headers);
   }
 }
